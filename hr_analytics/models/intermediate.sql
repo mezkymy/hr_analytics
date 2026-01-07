b4580fc16d94860867847a422ca0fb1f6bdbbeec
mez <mezkymy@gmail.com>
2026-01-07 10:34:13 +0700

    feat: intermediate model to clean null values & rows

--- hr_analytics/models/intermediate.sql
+++ hr_analytics/models/intermediate.sql
@@ -1,5 +1,18 @@
 -- clean data from staging
 
-select *
+select 
+    case
+        when lower(NIK_KARYAWAN) = 'nan' then null
+        else cast(NIK_KARYAWAN as varchar)
+    end as NIK_KARYAWAN
+    , PERUSAHAAN
+    , HIRE_DATE
+    , TERMINATION_DATE
+    , STATUS
 from {{ ref('staging') }}
-
+where 
+    ((NIK_KARYAWAN is not null) or (not lower(NIK_KARYAWAN) = 'nan'))
+    and (PERUSAHAAN is not null)
+    and (HIRE_DATE is not null)
+    and (TERMINATION_DATE is not null)
+    and (STATUS is not null)
